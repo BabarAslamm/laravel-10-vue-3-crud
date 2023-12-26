ee5e13f1ef255f1c67a76e2d0bf6e1f91dae8d77
babar <babar@bitsoftsol.com>
2023-12-26 15:04:09 +0500

    company crud completed

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+import { createApp } from 'vue';
+// import { createApp } from 'vue/dist/vue.esm-bundler';
+import router from './router'
+
+import CompaniesIndex from '@/components/companies/CompaniesIndex.vue';
+
+createApp({
+    components: {
+        CompaniesIndex
+    }
+}).use(router).mount('#app')
+
